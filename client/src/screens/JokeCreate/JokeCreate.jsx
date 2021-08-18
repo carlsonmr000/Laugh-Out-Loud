fb6a69dce5fb38c026004b9ecd68ae14f894ef6b
carlsonmr <carlsonmr000@gmail.com>
2021-08-17 20:17:30 -0500

    rendered create joke page

--- client/src/screens/JokeCreate/JokeCreate.jsx
+++ client/src/screens/JokeCreate/JokeCreate.jsx
@@ -1,77 +1,83 @@
-import { useState, useEffect } from 'react'
-import './JokeCreate.css'
+import { useState, useEffect } from "react";
+import "./JokeCreate.css";
 // import Layout from '../../components/Layout/Layout'
-import { Redirect } from 'react-router-dom'
+import { Redirect } from "react-router-dom";
 import { verify } from "../../services/users";
-import { createJoke } from '../../services/jokes'
+import { createJoke } from "../../services/jokes";
 
 const JokeCreate = (props) => {
+  const [joke, setJoke] = useState({
+    title: "",
+    content: "",
+    user_id: props.user?.id,
+  });
 
-    const [joke, setJoke] = useState({
-            title: '',
-            content: '',
-            user_id: props.user?.id
-        })
+  const [isCreated, setCreated] = useState(false);
+  const [userBool, setUserBool] = useState(null);
 
-    const [isCreated, setCreated] = useState(false)
-    const [userBool, setUserBool] = useState(null);
+  useEffect(() => {
+    const checkUser = async () => {
+      const userExists = await verify();
+      setUserBool(userExists ? true : false);
+    };
+    checkUser();
+  }, []);
 
-    useEffect(() => {
-        const checkUser = async () => {
-          const userExists = await verify();
-          setUserBool(userExists ? true : false);
-        };
-        checkUser();
-      }, []);
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setJoke({
+      ...joke,
+      [name]: value,
+    });
+  };
 
-      const handleChange = (e) => {
-        const { name, value } = e.target;
-        setJoke({
-          ...joke,
-          [name]: value,
-        });
-      };
-    
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const addJoke = async () => {
+      const created = await createJoke(joke);
+      setCreated({ created });
+    };
+    addJoke();
+  };
+  if (isCreated) {
+    return <Redirect to={`/jokes`} />;
+  }
 
-      const handleSubmit = (e) => {
-        e.preventDefault();
-        const addJoke = async () => {
-          const created = await createJoke(joke);
-          setCreated({ created });
-        }
-        addJoke();
-      };
-    if (isCreated) {
-        return <Redirect to={`/jokes`} />
-    }
+  return !userBool && userBool !== null ? (
+    <Redirect to="/log-in" />
+  ) : (
+    <div className="create-parent">
+      <form className="create-form" onSubmit={handleSubmit}>
+        <label htmlFor="title" className="create-title">
+          Title:
+        </label>
+        <input
+          className="input-title"
+          placeholder="Title"
+          value={joke.title}
+          name="title"
+          required
+          autoFocus
+          onChange={handleChange}
+        />
+        <label htmlFor="content" className="create-content">
+          Content:
+        </label>
+        <textarea
+          className="textarea-content"
+          rows={5}
+          placeholder="Content"
+          value={joke.content}
+          name="content"
+          required
+          onChange={handleChange}
+        />
+        <button type="submit" className="create-submit">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+};
 
-    return !userBool && userBool !== null ?(
-        <Redirect to="/log-in" />
-      ) : (
-        <>
-            <form className="create-form" onSubmit={handleSubmit}>
-                <input
-                    className="input-title"
-                    placeholder='Title'
-                    value={joke.title}
-                    name='title'
-                    required
-                    autoFocus
-                    onChange={handleChange}
-                />
-                <textarea
-                    className="textarea-content"
-                    rows={5}
-                    placeholder='Content'
-                    value={joke.content}
-                    name='content'
-                    required
-                    onChange={handleChange}
-                />
-                <button type='submit' className="submit-button">Submit</button>
-            </form>
-        </>
-    )
-}
-
-export default JokeCreate
+export default JokeCreate;
