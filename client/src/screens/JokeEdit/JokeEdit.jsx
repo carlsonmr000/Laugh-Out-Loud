7d06d2f5ef3ad88577b075c4d23403418adf9aa8
carlsonmr <carlsonmr000@gmail.com>
2021-08-17 21:16:22 -0500

    rendered edit joke page

--- client/src/screens/JokeEdit/JokeEdit.jsx
+++ client/src/screens/JokeEdit/JokeEdit.jsx
@@ -1,80 +1,84 @@
-import { useState, useEffect } from 'react'
-import './JokeEdit.css'
-import { useParams, Redirect, useHistory } from 'react-router-dom'
+import { useState, useEffect } from "react";
+import "./JokeEdit.css";
+import { useParams, Redirect, useHistory } from "react-router-dom";
 // import Layout from '../../components/Layout/Layout'
-import { getOneJoke, updateJoke } from '../../services/jokes'
+import { getOneJoke, updateJoke } from "../../services/jokes";
 
 const JokeEdit = (props) => {
   const [joke, setJoke] = useState({
-    title: '',
-    content: '',
-    user_id: props.user?.id
-  })
+    title: "",
+    content: "",
+    user_id: props.user?.id,
+  });
 
-
-
-  const params = useParams()
-  const history = useHistory()
+  const params = useParams();
+  const history = useHistory();
   // console.log(params.id)
 
-  const [isUpdated, setUpdated] = useState(false)
+  const [isUpdated, setUpdated] = useState(false);
 
   useEffect(() => {
     const fetchJoke = async (jokeID) => {
-      const joke = await getOneJoke(jokeID)
-      setJoke(joke)
-    }
-    fetchJoke(params.id)
-  }, [params.id])
+      const joke = await getOneJoke(jokeID);
+      setJoke(joke);
+    };
+    fetchJoke(params.id);
+  }, [params.id]);
 
   const handleChange = (event) => {
-    const { name, value } = event.target
+    const { name, value } = event.target;
     setJoke({
       ...joke,
       [name]: value,
-    })
-  }
+    });
+  };
 
   const handleSubmit = async (event) => {
-    event.preventDefault()
-    const updated = await updateJoke(params.id, joke)
-    setUpdated(updated)
-      history.push('/jokes')
-  }
+    event.preventDefault();
+    const updated = await updateJoke(params.id, joke);
+    setUpdated(updated);
+    history.push("/jokes");
+  };
 
   if (isUpdated) {
-    return <Redirect to={`/jokes`} />
+    return <Redirect to={`/jokes`} />;
   }
 
   return (
     <>
-      <div className='joke-edit'>
-        <form className='edit-form' onSubmit={handleSubmit}>
+      <div className="joke-edit-parent">
+        <form className="edit-form" onSubmit={handleSubmit}>
+          <label htmlFor="title" className="edit-title">
+            Title:
+          </label>{" "}
           <input
-            className='input-title'
-            placeholder='Title'
+            className="edit-input-title"
+            placeholder="Title"
             value={joke.title}
-            name='title'
+            name="title"
             required
             autoFocus
             onChange={handleChange}
           />
+          <label htmlFor="content" className="edit-content">
+            Content:
+          </label>{" "}
           <textarea
-            className='textarea-content'
+            className="edit-textarea-content"
             rows={5}
-            placeholder='content'
+            placeholder="content"
             value={joke.content}
-            name='content'
+            name="content"
             required
             onChange={handleChange}
           />
-          <button type='submit' className='save-button'>
+          <button type="submit" className="edit-save">
             Save
           </button>
         </form>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default JokeEdit
+export default JokeEdit;
