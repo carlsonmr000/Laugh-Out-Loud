42d6d9c3c480151949971869753082fae2ea6244
carlsonmr <carlsonmr000@gmail.com>
2021-08-18 09:09:21 -0500

    updated log in and sign up inputs

--- client/src/screens/JokeDetail/JokeDetail.jsx
+++ client/src/screens/JokeDetail/JokeDetail.jsx
@@ -1,65 +1,72 @@
-import { useState, useEffect } from 'react'
-import './JokeDetail.css'
-// import Layout from '../../components/Layout/Layout'
-import { getOneJoke, deleteJoke } from '../../services/jokes'
-import { useParams, useHistory } from 'react-router-dom'
+import { useState, useEffect } from "react";
+import "./JokeDetail.css";
+import { getOneJoke, deleteJoke } from "../../services/jokes";
+import { useParams, useHistory } from "react-router-dom";
 
 const JokeDetail = (props) => {
+  const [user, setUser] = useState({});
+  const [joke, setJoke] = useState([]);
+  const [isLoaded, setLoaded] = useState(false);
+  const history = useHistory();
+  const { id } = useParams();
 
-    const [user, setUser] = useState({})
-    const [joke, setJoke] = useState([])
-    const [isLoaded, setLoaded] = useState(false)
-     const history = useHistory();
-    const { id } = useParams()
- 
+  useEffect(() => {
+    const fetchJoke = async (jokeID) => {
+      const joke = await getOneJoke(jokeID);
+      setJoke(joke);
+      setUser(props.user);
+      setLoaded(true);
+    };
+    fetchJoke(id);
+  }, [id, props.user]);
 
-    useEffect(() => {
-        const fetchJoke = async (jokeID) => {
-            const joke = await getOneJoke(jokeID)
-            setJoke(joke)
-            setUser(props.user)
-            setLoaded(true)
-        }
-        fetchJoke(id)
-    }, [id, props.user])
+  if (!isLoaded) {
+    return <h1>Loading...</h1>;
+  }
 
-    if (!isLoaded) {
-        return <h1>Loading...</h1>
-    }
+  const handleSubmit = () => {
+    const deleteOneJoke = async () => {
+      await deleteJoke(id);
+      setTimeout(() => {
+        history.push("/jokes");
+      }, 500);
+    };
+    deleteOneJoke();
+  };
 
-    const handleSubmit = () => {
-        const deleteOneJoke = async () => {
-          await deleteJoke(id);
-          setTimeout(() => {
-            history.push("/jokes");
-          }, 500);
-        };
-        deleteOneJoke();
-      };
+  const handleClick = () => {
+    history.push(`/jokes/${id}/edit`);
+  };
 
-     const handleClick = () => {
-        history.push(`/jokes/${id}/edit`) 
-      }
-    
-     return (
-        <div>
-            
-            <div className="joke-detail">
-                    <div className="detail-title">{joke.title}</div>
-                    <div className="content-container">
-                    <div className="detail-content">{joke.content}</div>
-                    </div>
-                    <div className="button-container">
-
-            {user ? <>  <button className="detail-edit" onClick={handleClick}>Edit</button> 
-                              <button className="detail-delete" to="/jokes" onClick={handleSubmit}>Delete</button></> : <></>}
-                    </div>
-            </div>
+  return (
+    <div>
+      <div className="joke-detail">
+        <div className="detail-title">{joke.title}</div>
+        <div className="content-container">
+          <div className="detail-content">{joke.content}</div>
         </div>
-    )
-}
+        <div className="button-container">
+          {user ? (
+            <>
+              {" "}
+              <button className="detail-edit" onClick={handleClick}>
+                Edit
+              </button>
+              <button
+                className="detail-delete"
+                to="/jokes"
+                onClick={handleSubmit}
+              >
+                Delete
+              </button>
+            </>
+          ) : (
+            <></>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default JokeDetail
-{/* <button className="edit-button"><Link className="edit-link" to={`/jokes/${id}/edit`}>Edit</Link></button> */}
-
-    {/* <button className="add-joke-button" onClick={handleClick}>Add a joke</button> */}
+export default JokeDetail;
